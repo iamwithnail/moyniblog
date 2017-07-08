fe1884587c3d326c111536999d3c9d1317f5ec35
Chris Campbell <chris@ccampbell.co>
2017-07-08 21:00:55 +0100

    Update fabfile to run all

--- fabfile.py
+++ fabfile.py
@@ -12,10 +12,17 @@ def push():
     local("git push origin master")
 
 
-def deploy():
+def server_work():
     code_dir = '/var/www/moyniblog/blog'
     with cd(code_dir):
         run("git pull")
+        run("source ../env/bin/activate")
+        run("python manage.py collectstatic --noinput")
         run("sudo service gunicorn restart")
 
         #/var/www/moyniblog/env/bin/gunicorn --workers 3 --bind 127.0.0.1:8099 --reload=True
+
+def deploy():
+    prepare_deploy()
+    push()
+    server_work()
