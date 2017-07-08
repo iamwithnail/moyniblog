6d5f5872fa91916f777e533737e5a2a40f658eb7
Chris Campbell <chris@ccampbell.co>
2017-07-08 20:22:32 +0100

    Fabfile initial commit.

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import local, run, env, cd
+
+
+env.hosts = ['blog.moynibell.com']
+def prepare_deploy():
+    local("git add -p && git commit")
+    local("git push origin master")
+
+def deploy():
+    code_dir = '/var/www/moyniblog/blog'
+    with cd(code_dir):
+        run("git pull")
+        run("sudo service gunicorn restart")
+
+        #/var/www/moyniblog/env/bin/gunicorn --workers 3 --bind 127.0.0.1:8099 --reload=True
