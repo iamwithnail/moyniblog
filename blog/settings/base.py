6cdf68d59d022d24025359e4f16b4f95c749363b
Chris Campbell <chris@ccampbell.co>
2018-08-02 16:00:46 +0100

    Branching for layout changes.

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -93,10 +93,15 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moyniblog',
+        'USER': 'moyniblogger',
+        'PASSWORD': 'U8wKlTRaI6!&j%',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
